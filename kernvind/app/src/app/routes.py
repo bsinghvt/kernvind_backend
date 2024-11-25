9dba2a9b687790716f42de99320de3bfc8bc21da
bsinghvt <12547582+bsinghvt@users.noreply.github.com>
2024-11-25 11:49:04 -0800

    added api prefix for url paths

--- kernvind/app/src/app/routes.py
+++ kernvind/app/src/app/routes.py
@@ -8,10 +8,10 @@ from .datasource.controllers.datasource_controller import datasource_bp
 
 from quart import Quart
 def register_routes(app: Quart):
-    app.register_blueprint(social_auth_bp, url_prefix='/auth')
-    app.register_blueprint(signup_bp, url_prefix='/auth')
-    app.register_blueprint(login_bp, url_prefix='/auth')
-    app.register_blueprint(bot_bp, url_prefix='/bot')
-    app.register_blueprint(user_llm_bp, url_prefix='/userllm')
-    app.register_blueprint(public_llm_bp, url_prefix='/publicllm')
-    app.register_blueprint(datasource_bp, url_prefix='/datasource') 
+    app.register_blueprint(social_auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(signup_bp, url_prefix='/api/auth')
+    app.register_blueprint(login_bp, url_prefix='/api/auth')
+    app.register_blueprint(bot_bp, url_prefix='/api/bot')
+    app.register_blueprint(user_llm_bp, url_prefix='/api/userllm')
+    app.register_blueprint(public_llm_bp, url_prefix='/api/publicllm')
+    app.register_blueprint(datasource_bp, url_prefix='/api/datasource') 
