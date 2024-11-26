c803ea95736298edc5bec6c5bea3e520f46a03d5
bsinghvt <12547582+bsinghvt@users.noreply.github.com>
2024-11-26 10:49:21 -0800

    Added kernvind service build script

---
+++ kernvind_service_build.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SERVICE_NAME=kernvind
+IMAGE=kernvind/api_services
+REPO=539247470170.dkr.ecr.us-east-2.amazonaws.com
+docker build --build-arg SERVICE_NAME=$SERVICE_NAME -f $SERVICE_NAME/Dockerfile -t $IMAGE .
+docker tag $IMAGE/latest $REPO/$IMAGE:latest
+docker push $REPO/$IMAGE:latest
