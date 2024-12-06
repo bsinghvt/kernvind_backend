599350ce69b89f9d1c485f3d0b955d583516738e
bsinghvt <12547582+bsinghvt@users.noreply.github.com>
2024-12-05 16:11:04 -0800

    Updated build scripts

--- kernvind_service_build.bash
+++ kernvind_service_build.bash
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 539247470170.dkr.ecr.us-east-1.amazonaws.com
 SERVICE_NAME=kernvind
 IMAGE=kernvind/api_services
-REPO=539247470170.dkr.ecr.us-east-2.amazonaws.com
+#DOCKER_REO=baljotsingh/kernvind-api-service
+#DOCKER_IMAGE_TAG=api
+REPO=539247470170.dkr.ecr.us-east-1.amazonaws.com
 docker build --build-arg SERVICE_NAME=$SERVICE_NAME -f $SERVICE_NAME/Dockerfile -t $IMAGE .
 docker tag $IMAGE:latest $REPO/$IMAGE:latest
 docker push $REPO/$IMAGE:latest
+#docker tag $IMAGE:latest $DOCKER_REO:$DOCKER_IMAGE_TAG
+#docker push $DOCKER_REO:$DOCKER_IMAGE_TAG
--- llm_chat_service_build.bash
+++ llm_chat_service_build.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 539247470170.dkr.ecr.us-east-1.amazonaws.com
 docker build --build-arg SERVICE_NAME=llm_chat_service -f llm_chat_service/Dockerfile  -t kernvind/chat_service .
-docker tag kernvind/chat_service:latest 539247470170.dkr.ecr.us-east-2.amazonaws.com/kernvind/chat_service:latest
-docker push 539247470170.dkr.ecr.us-east-2.amazonaws.com/kernvind/chat_service:latest
+docker tag kernvind/chat_service:latest 539247470170.dkr.ecr.us-east-1.amazonaws.com/kernvind/chat_service:latest
+docker push 539247470170.dkr.ecr.us-east-1.amazonaws.com/kernvind/chat_service:latest
