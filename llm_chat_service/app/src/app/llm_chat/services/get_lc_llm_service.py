1c192ab18e3891a357ab5166e99fcec0b23a0f09
bsinghvt <12547582+bsinghvt@users.noreply.github.com>
2024-12-07 22:14:49 -0800

    Added dict for user llm name and Ollama auth

--- llm_chat_service/app/src/app/llm_chat/services/get_lc_llm_service.py
+++ llm_chat_service/app/src/app/llm_chat/services/get_lc_llm_service.py
@@ -11,14 +11,34 @@ def get_lc_llm(bot_user_llm: BotUserLlm):
     bot_id = bot_user_llm.bot_id
     user_llm_id = bot_user_llm.user_llm_id
     llm_instance_dict = current_app.config['LC_LLM_INSTANCE_COLLECTION']
+    llm_user_llm_name_dict = current_app.config['LC_USER_LLM_NAME_INSTANCE_COLLECTION']
+    create_new = False
+    if user_llm_id in llm_user_llm_name_dict and bot_user_llm.llm_name != llm_user_llm_name_dict[user_llm_id]:
+        create_new = True
+    llm_user_llm_name_dict[user_llm_id] = bot_user_llm.llm_name
 
-    if user_llm_id not in llm_instance_dict or not llm_instance_dict[user_llm_id]:
+    if user_llm_id not in llm_instance_dict or not llm_instance_dict[user_llm_id] or create_new:
         try:
             if bot_user_llm.llmmodeltype_name == OLLAMA:
+                auth = None
+                headers = None
+                if bot_user_llm.llm_config:
+                    if bot_user_llm.llm_config.user_name and bot_user_llm.llm_config.password:
+                        auth=(bot_user_llm.llm_config.user_name, bot_user_llm.llm_config.password)
+                    if bot_user_llm.llm_config.api_key and bot_user_llm.llm_config.api_key_header:
+                        headers = {
+                            bot_user_llm.llm_config.api_key : bot_user_llm.llm_config.api_key_header
+                            }
+                client_kwargs = {}
+                if auth:
+                    client_kwargs['auth'] = auth
+                if headers:
+                    client_kwargs['headers'] = headers
                 llm_instance_dict[user_llm_id] = ChatOllama(
                     model = bot_user_llm.llm_name,
                     temperature=0,
                     base_url = bot_user_llm.llm_config.url,
+                    client_kwargs = client_kwargs
                 )
             elif bot_user_llm.llmmodeltype_name == OPENAI:
                 llm_instance_dict[user_llm_id] = ChatOpenAI(
